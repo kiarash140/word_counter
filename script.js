413b14bf3de2bd21962a711118b284699c8c89a8
kiarash140 <22527887kiarash@gmail.com>
2025-08-12 16:01:44 +0330

    css accept

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log('Happy developing ✨')
+let inputel=document.querySelector('#input');
+const facebook=2200;
+const twitter=280;
+inputel.addEventListener('input', function(){
+   let paragraph=inputel.value;
+   document.querySelector('#charecter_count_show').textContent=paragraph.length;
+
+   let twitter_count_showel=twitter-paragraph.length;
+   document.querySelector('#twitter_count_show').textContent=twitter_count_showel;
+    let facebook_count_showel=facebook-paragraph.length;
+    document.querySelector('#facebook_count_show').textContent=facebook_count_showel;
+    const words = (s) => s.trim().split(/\s+/).length;
+    document.querySelector('#word_count_show').textContent=words(paragraph);
+    if (paragraph.length===0){
+        document.querySelector('#word_count_show').textContent=0;
+
+    }
+    if(paragraph.length>=280){
+        document.querySelector('#twitter').style.backgroundColor="red";
+    }
+    else{
+        document.querySelector('#twitter').style.backgroundColor="#F1F6F8FF";
+    }
+    if(paragraph.length>=2200){
+        document.querySelector('#facebook').style.backgroundColor="red";
+    }
+    else{
+        document.querySelector('#facebook').style.backgroundColor="#F1F6F8FF";
+    }
+
+});
